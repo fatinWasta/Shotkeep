4f9e6dd52c658cca74a01c26be34b2daff6b6357
fatin41 <fatin@particle41.com>
2026-02-21 07:44:38 +0530

    Initial Commit

---
+++ ShotKeep/ShotKeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShotKeepApp.swift
+//  ShotKeep
+//
+//  Created by Fatin on 21/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ShotKeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
